28fc3ee84c05faf95eab8fbf94bc27b26994e924
wheeeung <whee050916@gmail.com>
2022-07-27 14:26:28 +0900

    delete

--- src/main/java/com/example/mmproject/domain/chat/controller/ChatController.java
+++
@@ -1,4 +0,0 @@
-package com.example.mmproject.domain.chat.controller;
-
-public class ChatController {
-}
--- src/main/java/com/example/mmproject/domain/chat/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mmproject.domain.chat.repository;
-
-import com.example.mmproject.domain.chat.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
