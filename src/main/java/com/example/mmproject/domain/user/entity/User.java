7a0b6519a91e2a605552b66fc3eaa6ef77ddeca1
beargame123 <hoyoung7827@gmail.com>
2022-07-28 22:37:22 +0900

    🛠️:: User set메소드 추가

--- src/main/java/com/example/mmproject/domain/user/entity/User.java
+++ src/main/java/com/example/mmproject/domain/user/entity/User.java
@@ -24,20 +24,36 @@ public class User {
     private List<Project> projects;
 
     @Column
-    private String name;
+    private String name; // 닉넴
 
     @Column
-    private String email;
+    private String email; // 이메일
 
     @Column
-    private String password;
+    private String password; // 비밀번호
 
     @Column
-    private String introduction;
+    private String introduction; // 한줄소개
 
     @Column
-    private String jobGroup;
+    private String jobGroup; // 직군
 
     @Column
-    private String githubLink;
+    private String language; // 언어
+
+    @Column
+    private String githubLink; // 깃허브 링크
+
+    // 비밀번호 빼고 수정
+    public void setMyPage(String introduction, String jobGroup, String language, String githubLink){
+        introduction = this.introduction;
+        jobGroup = this.jobGroup;
+        language = this.language;
+        githubLink = this.githubLink;
+    }
+
+    // 비밀번호 수정
+    public void setPassword(String password){
+        password = this.password;
+    }
 }
