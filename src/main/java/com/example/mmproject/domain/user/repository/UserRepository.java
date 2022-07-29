b83ed5e993b8933a904534468af6e9525f67f473
wheeeung <whee050916@gmail.com>
2022-07-29 14:42:42 +0900

    delete post and user

--- src/main/java/com/example/mmproject/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mmproject.domain.user.repository;
-
-import com.example.mmproject.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
