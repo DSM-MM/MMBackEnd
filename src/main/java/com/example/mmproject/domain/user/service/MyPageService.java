271a920b3f39df95b8ffedc72f6be1be317ca0a1
beargame123 <hoyoung7827@gmail.com>
2022-08-03 12:37:30 +0900

    🛠️:: userService 수정

--- src/main/java/com/example/mmproject/domain/user/service/MyPageService.java
+++ src/main/java/com/example/mmproject/domain/user/service/MyPageService.java
@@ -23,33 +23,40 @@ public class MyPageService {
     }
 
     // 마이페이지 수정
-    @Transactional
     public void setMyPage(Long id, UserRequest userRequest){
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("USER_ID_NOT_FOUND"));
-        user.setMyPage(userRequest.getIntroduction(), userRequest.getJobGroup(), userRequest.getLanguage(), userRequest.getGithubLink());
+        user.setMyPage(userRequest.getName(),userRequest.getIntroduction(), userRequest.getJobGroup(), userRequest.getLanguage(), userRequest.getGithubLink());
         userRepository.save(user);
     }
 
     // 이전비밀번호 검사, 새로운 비밀번호 검사
-    @Transactional
     public void setPassword(Long id, UserRequest userRequest) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("USER_ID_NOT_FOUND"));
-        try {
+
+        // 구글 사용자인지 아닌지
+        if(user.getProvider() == null) {
             // 이전 비밀번호 맞는지 검사
-            if (passwordEncoder.matches(user.getPassword(),userRequest.getPassword())) {
-                try {
-                    // 변경할 비밀번호를 입력을 잘했는지 검사
-                    if (userRequest.getNewPassword().equals(userRequest.getNewPasswordValid())) {
-                        user.setPassword(passwordEncoder.encode(userRequest.getNewPassword()));
-                        userRepository.save(user);
-                        log.info("변경 되었습니다.");
-                    } else throw new Exception();
-                } catch (Exception e) {
-                    log.error("USER_NEW_PASSWORD_NOT_EQUALS");
-                }
-            } else throw new Exception();
-        } catch (Exception e) {
-            log.error("USER_ORIGINAL_PASSWORD_NOT_EQUALS");
+            if (passwordEncoder.matches(userRequest.getPassword(), user.getPassword())){
+                // 변경할 비밀번호를 입력을 잘했는지 검사
+                if (userRequest.getNewPassword().equals(userRequest.getNewPasswordValid())) {
+                    user.setPassword(passwordEncoder.encode(userRequest.getNewPassword()));
+                    userRepository.save(user);
+                    log.info("USER_PASSWORD_CHANGE_SUCCESS");
+                } else log.error("USER_NEW_PASSWORD_NOT_EQUALS");
+
+            } else log.error("USER_ORIGINAL_PASSWORD_NOT_EQUALS");
+        }else{
+            // 이전 비밀번호 맞는지 검사
+            if (userRequest.getPassword().equals(user.getPassword())){
+                // 변경할 비밀번호를 입력을 잘했는지 검사
+                if (userRequest.getNewPassword().equals(userRequest.getNewPasswordValid())) {
+                    user.setPassword(passwordEncoder.encode(userRequest.getNewPassword()));
+                    userRepository.save(user);
+                    log.info("USER_PASSWORD_CHANGE_SUCCESS");
+                } else log.error("USER_NEW_PASSWORD_NOT_EQUALS");
+
+            } else log.error("USER_ORIGINAL_PASSWORD_NOT_EQUALS");
         }
+
     }
 }
