d373ea5806e4735f0f9c9fd8daa64b628c98c94b
beargame123 <hoyoung7827@gmail.com>
2022-08-03 12:22:29 +0900

    🛠️:: X-AUTH-TOKEN -> ACESS_TOKEN

--- src/main/java/com/example/mmproject/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/mmproject/global/security/jwt/JwtTokenProvider.java
@@ -89,8 +89,8 @@ public class JwtTokenProvider {
         }
     }
 
-    // Request의 Header 에서 token값을 가져온다. "X-AUTH-TOKEN" :"TOKEN값"
-    public String resolveToken(HttpServletRequest request){return request.getHeader("X-AUTH-TOKEN");}
+    // Request의 Header 에서 token값을 가져온다. "ACCESS_TOKEN" :"TOKEN값"
+    public String resolveToken(HttpServletRequest request){return request.getHeader("ACCESS_TOKEN");}
 
     // 토큰의 유효성 + 만료일자 확인
     public boolean validateToken(String jwtToken){
