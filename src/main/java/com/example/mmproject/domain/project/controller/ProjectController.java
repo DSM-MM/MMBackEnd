63e2f4c85f586ea5e2abc8d10c8cdf10d36a9090
wheeeung <whee050916@gmail.com>
2022-09-13 20:06:30 +0900

    add s3upload

--- src/main/java/com/example/mmproject/domain/project/controller/ProjectController.java
+++ src/main/java/com/example/mmproject/domain/project/controller/ProjectController.java
@@ -3,14 +3,17 @@ package com.example.mmproject.domain.project.controller;
 import com.example.mmproject.domain.project.controller.dto.ProjectRequest;
 import com.example.mmproject.domain.project.entity.Project;
 import com.example.mmproject.domain.project.service.ProjectService;
+import com.example.mmproject.domain.project.service.S3Upload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final S3Upload s3Upload;
 
     @PostMapping("/project")
     public Project create(@RequestBody ProjectRequest request){
@@ -30,5 +33,10 @@ public class ProjectController {
     @GetMapping("/project/{id}")
     public Project detail(@PathVariable Long id){
         return projectService.detail(id);
+    }
+
+    @PostMapping("/project/upload")
+    public String upload(MultipartFile multipartFile) throws Exception{
+        return s3Upload.upload(multipartFile);
     }
 }
