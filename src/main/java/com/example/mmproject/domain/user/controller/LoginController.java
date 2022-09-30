7dd2ca18ce63d3d9c3fabe7c86d28c2fcd0b22c3
beargame123 <hoyoung7827@gmail.com>
2022-09-30 09:39:30 +0900

    ➕:: 회원 삭제 추가

--- src/main/java/com/example/mmproject/domain/user/controller/LoginController.java
+++ src/main/java/com/example/mmproject/domain/user/controller/LoginController.java
@@ -3,20 +3,27 @@ package com.example.mmproject.domain.user.controller;
 import com.example.mmproject.domain.user.controller.dto.UserRequest;
 import com.example.mmproject.domain.user.entity.User;
 import com.example.mmproject.domain.user.repository.UserRepository;
+import com.example.mmproject.domain.user.service.MyPageService;
 import com.example.mmproject.global.security.auth.enums.Role;
 import com.example.mmproject.global.security.jwt.JwtTokenProvider;
 import com.example.mmproject.global.security.jwt.dto.TokenDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
+    private final MyPageService myPageService;
     private final JwtTokenProvider jwtProvider;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -34,6 +41,11 @@ public class LoginController {
                 .role(Role.ROLE_USER).build());
     }
 
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect/logout";
+    }
 
     @PostMapping("/login")
     public TokenDto login(@RequestBody UserRequest dto){
@@ -43,5 +55,13 @@ public class LoginController {
             throw new IllegalStateException("잘못된 비밀번호 입니다.");
         }
         return jwtProvider.createToken(user.getEmail(), user.getRole());
+    }
+
+    @DeleteMapping("/leave")
+    public void leaveUser(Authentication authentication){
+        if(authentication == null){
+            throw new BadCredentialsException("USER_INFO_NOT_FOUND");
+        }
+        myPageService.leaveUser(authentication.getName());
     }
 }
