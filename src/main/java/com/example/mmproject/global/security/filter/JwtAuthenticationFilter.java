07dd224d98f634d81a7b0bb66a878c153d444a4e
beargame123 <hoyoung7827@gmail.com>
2022-09-30 10:30:03 +0900

    🛠️:: filter 로직 변경

--- src/main/java/com/example/mmproject/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/mmproject/global/security/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.example.mmproject.global.security.filter;
 
+import com.example.mmproject.domain.user.controller.dto.UserDto;
 import com.example.mmproject.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -13,28 +16,40 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends GenericFilterBean{
     private final JwtTokenProvider jwtTokenProvider;
 
-    // request 로 들어가는 Jwt 의 유효성 검증
+    // request 로 들어가는 Jwt의 유효성 검증
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // request 에서 token을 취함.
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        // request 에서 token 취함.
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
 
         // 검증
-        log.info("토큰을 확인하는중...");
+        log.info("토큰을 확인하는중");
         log.info(((HttpServletRequest) request).getRequestURI());
 
-        if (token != null && jwtTokenProvider.validateToken(token)){
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 가져옴.
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SpringContext 에 Authentication 객체를 가져옴.
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if(token != null && jwtTokenProvider.validateToken(token)){
+
+            String email = jwtTokenProvider.getUid(token);
+            // DB연동을 안했으니 이메일 정보로 유저를 만들어주겠습니다
+            UserDto userDto = UserDto.builder()
+                    .email(email)
+                    .name("이름이에용").build();
+
+            Authentication auth = getAuthentication(userDto);
+            SecurityContextHolder.getContext().setAuthentication(auth);
         }
-        chain.doFilter(request,response);
+
+        chain.doFilter(request, response);
+    }
+
+    public Authentication getAuthentication(UserDto member) {
+        return new UsernamePasswordAuthenticationToken(member, "",
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
     }
 }
