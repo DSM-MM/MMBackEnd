7bd1c922896e78e139f57db3d37c5dbe5715dd50
beargame123 <hoyoung7827@gmail.com>
2022-09-30 09:58:01 +0900

    ➕:: Oauth2Service 추가

--- src/main/java/com/example/mmproject/global/security/auth/AuthDetailService.java
+++ src/main/java/com/example/mmproject/global/security/auth/AuthDetailService.java
@@ -1,15 +1,22 @@
 package com.example.mmproject.global.security.auth;
 
+import com.example.mmproject.domain.user.entity.User;
 import com.example.mmproject.domain.user.repository.UserRepository;
+import com.example.mmproject.global.security.auth.enums.Role;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
 @RequiredArgsConstructor
 @Service
-public class AuthDetailService implements UserDetailsService {
+public class AuthDetailService extends DefaultOAuth2UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
@@ -17,5 +24,38 @@ public class AuthDetailService implements UserDetailsService {
         return userRepository.findByEmail(email)
                 .map(AuthDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("NOT_FOUND_USER"));
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        // DefaultOAuth2UserService: 객체를 성공정보를 바탕으로 만든다.
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+
+        // 생성된 Service 객체로 부터 User를 받는다.
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        // 받은 User로 부터 user 정보를 받는다.
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        log.info("registrationId = {}", registrationId);
+        log.info("userNameAttributeName = {}", userNameAttributeName);
+
+        if(!userRepository.existsByEmail(oAuth2User.getAttribute("email"))){
+            userRepository.save(User.builder()
+                    .email(oAuth2User.getAttribute("email"))
+                    .name(oAuth2User.getAttribute("name"))
+                    .picture(oAuth2User.getAttribute("picture"))
+                    .role(Role.ROLE_USER)
+                    .provider(userRequest.getClientRegistration().getRegistrationId())
+                    .build());
+        }else{
+            User user = userRepository.findByEmail(oAuth2User.getAttribute("email")).orElseThrow(()-> new IllegalArgumentException("NOT_FOUND"));
+            user.update(oAuth2User.getAttribute("name"), oAuth2User.getAttribute("picture"));
+            userRepository.save(user);
+        }
+
+        return super.loadUser(userRequest);
     }
 }
