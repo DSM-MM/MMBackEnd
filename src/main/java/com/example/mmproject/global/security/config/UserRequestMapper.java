3b72d3ddf41d050af6e06778e5c75877743dc642
beargame123 <hoyoung7827@gmail.com>
2022-09-30 10:27:09 +0900

    ➕:: picture 추가

--- src/main/java/com/example/mmproject/global/security/config/UserRequestMapper.java
+++ src/main/java/com/example/mmproject/global/security/config/UserRequestMapper.java
@@ -1,16 +1,18 @@
 package com.example.mmproject.global.security.config;
 
-import com.example.mmproject.domain.user.entity.User;
+import com.example.mmproject.domain.user.controller.dto.UserDto;
+import com.example.mmproject.domain.user.controller.dto.UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRequestMapper {
-    public User toDto(OAuth2User oAuth2User) {
+    public UserDto toDto(OAuth2User oAuth2User) {
         var attributes = oAuth2User.getAttributes();
-        return User.builder()
+        return UserDto.builder()
                 .email((String)attributes.get("email"))
                 .name((String)attributes.get("name"))
+                .picture((String)attributes.get("picture"))
                 .build();
     }
 }
