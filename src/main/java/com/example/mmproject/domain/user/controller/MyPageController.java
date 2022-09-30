54f8609095ee5f53ea5e17d876de96fc4b136259
beargame123 <hoyoung7827@gmail.com>
2022-09-30 09:41:46 +0900

    🛠️:: token 인증 로직

--- src/main/java/com/example/mmproject/domain/user/controller/MyPageController.java
+++ src/main/java/com/example/mmproject/domain/user/controller/MyPageController.java
@@ -4,6 +4,8 @@ import com.example.mmproject.domain.user.controller.dto.UserRequest;
 import com.example.mmproject.domain.user.entity.User;
 import com.example.mmproject.domain.user.service.MyPageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,20 +15,24 @@ public class MyPageController {
     private final MyPageService myPageService;
 
     // 마이페이지 보기
-    @GetMapping("/{id}")
-    public User getMyPage(@PathVariable Long id){
-        return myPageService.getMyPage(id);
+    @GetMapping()
+    public User getMyPage(Authentication authentication){
+        if(authentication == null){
+            throw new BadCredentialsException("USER_NOT_FOUND");
+        }
+        return myPageService.getMyPage(authentication.getName());
     }
 
     // 마이페이지 수정
-    @PatchMapping("/{id}")
-    public void setMyPage(@PathVariable Long id, @RequestBody UserRequest request){
-        myPageService.setMyPage(id, request);
+    @PatchMapping("/set")
+    public void setMyPage(Authentication authentication, @RequestBody UserRequest request){
+        myPageService.setMyPage(authentication.getName(), request);
     }
 
     // 마이페이지 비밀번호 수정
-    @PatchMapping("/password/{id}")
-    public void setMyPassword(@PathVariable Long id, @RequestBody UserRequest request){
-        myPageService.setPassword(id, request);
+    @PatchMapping("/password")
+    public void setMyPassword(Authentication authentication, @RequestBody UserRequest request){
+        myPageService.setPassword(authentication.getName(), request);
     }
+
 }
