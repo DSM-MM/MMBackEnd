33a9fbfc84cdc7a2cbae8af018f35e962c6bc5b4
beargame123 <hoyoung7827@gmail.com>
2022-09-30 09:38:56 +0900

    .

--- src/main/java/com/example/mmproject/domain/project/service/ProjectService.java
+++ src/main/java/com/example/mmproject/domain/project/service/ProjectService.java
@@ -1,18 +1,30 @@
 package com.example.mmproject.domain.project.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.example.mmproject.domain.project.controller.dto.ProjectRequest;
+import com.example.mmproject.domain.project.entity.Image;
 import com.example.mmproject.domain.project.entity.Project;
+import com.example.mmproject.domain.project.repository.ImageRepository;
 import com.example.mmproject.domain.project.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
 
+    private final AmazonS3 amazonS3;
     private final ProjectRepository projectRepository;
+    private final ImageRepository imageRepository;
 
     //프로젝트 공고글 작성
     @Transactional
@@ -46,4 +58,21 @@ public class ProjectService {
     public Project detail(Long id){
         return projectRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id was wrong"));
     }
+
+    @Transactional
+    public String upload(MultipartFile multipartFile) throws IOException {
+        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        Image saveImage = new Image(s3FileName);
+
+        imageRepository.save(saveImage);
+
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentLength(multipartFile.getInputStream().available());
+
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+
+        return amazonS3.getUrl(bucket, s3FileName).toString();
+    }
+}
 }
