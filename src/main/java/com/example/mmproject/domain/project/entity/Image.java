bb7cb6048a7c0be17cce263b41e124645c3735ae
beargame123 <hoyoung7827@gmail.com>
2022-09-30 09:38:35 +0900

    .

---
+++ src/main/java/com/example/mmproject/domain/project/entity/Image.java
@@ -0,0 +1,32 @@
+package com.example.mmproject.domain.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "IMAGE")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public Image(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+}
